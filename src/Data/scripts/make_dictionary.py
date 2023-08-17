2eff40bc7b9ede94e20f469f9e449bd793da2709
airco879 <airco879@student.otago.ac.nz>
2023-08-17 20:10:58 +1200

    cleaned up commenting

--- src/Data/scripts/make_dictionary.py
+++ src/Data/scripts/make_dictionary.py
@@ -11,30 +11,29 @@ pos_tags = ['a', 'ad', 'arch', 'art', 'conj', 'def', 'fig', 'freq', 'ib', 'indef
 
 def main():
     maori_to_eng = {}
-    inverted_dict = {}
     
-    with open('testscraper.txt', 'w', encoding='utf-8') as output_file:
+    with open('check_output.txt', 'w', encoding='utf-8') as output_file:
 
         for page_number in range(1, MAX_PAGE_NUMBER+1):
             url = f'https://nzetc.victoria.ac.nz/tm/scholarly/tei-WillDict-t1-body-d1-d{page_number}.html'
             
             # Send an HTTP GET request to the URL
             response = requests.get(url)
-
             print(response.status_code)
-            # Check if the request was successful
+            
             if response.status_code == 200:
-                # Parse the HTML content of the page
                 soup = BeautifulSoup(response.content, 'html.parser')
                 
-                # Find elements with a specific class
+                # Find elements with a specific class, denotes each new word
                 section_elements = soup.find_all(class_='section')
                 
                 for word in section_elements:
                     
+                    #Skip outlier class="section" at start of every page.
                     if "tei-WillDict" in word['id']:
                         continue
                     
+                    # retrieve maori words from page, class="hang" contains one maori word, with multiple spelling(s)/variation(s)
                     translation_section = word.find(class_="hang")
                     maori_words = retrieve_maori_words(translation_section)
                     if maori_words == None:
@@ -46,34 +45,41 @@ def main():
 
                     
                     for word in maori_words:
-                        # output_file.write(word+'\n')
-                        
                         for d in e_definitions:
                             if word in maori_to_eng:
-                                maori_to_eng[word].append(d)
+                                maori_to_eng[word].append(d.lower())
                             else:
-                                maori_to_eng[word] = [d]
+                                maori_to_eng[word] = [d.lower()]
             else:
                 print('Failed to retrieve the webpage')
         
-        
-        
 
-        for key, values in maori_to_eng.items():
-            for value in values:
-                if value in inverted_dict:
-                    inverted_dict[value].append(key)
-                else:
-                    inverted_dict[value] = [key]
+        english_to_maori = create_inverted_dictionary(maori_to_eng)
         
-        for k,v in inverted_dict.items():
-            print(k,v)
+        # # write to file for checking.
+        # for k,v in english_to_maori.items():
+        #     output_file.write(k + " " + str(v) + "\n")
             
-    filename = "../clean_data/english_to_maori_dictionary_old.json"
+    filename = "../clean_data/english_to_maori_dictionary.json"
     with open(filename, "w") as file:
-        json.dump(inverted_dict, file)
+        json.dump(english_to_maori, file)
+
+def create_inverted_dictionary(dictionary):
+    inverted_dict = {}
+
+    for key, values in dictionary.items():
+        for value in values:
+            if value in inverted_dict:
+                inverted_dict[value].append(key)
+            else:
+                inverted_dict[value] = [key]
+    
+    return inverted_dict
 
 def retrieve_english_definitions(word):
+    
+    # find all html tags with a translation (itallics)
+    # again, some garbage gets in as HTML is inconsistent. This is okay for english, it just won't get matched.
     i_tags = word.find_all("i")
     if not i_tags:
         return None
@@ -90,26 +96,30 @@ def retrieve_english_definitions(word):
             
 
 def retrieve_maori_words(translation_section):
+    # look for foreign bolds, aka maori words with emphasis.
     try:
         maori_word = translation_section.find_next(class_="foreign bold").get_text()
     except:
         return None
     
+    # ignore any troubling translations
     bad_chars = ["‖", "(", "——"]
     if any(char in maori_word for char in bad_chars):
         return None
     
+    # trim garbage, html is inconsitent on what is allowed in tags.
     if maori_word[-1] in [":", ";"]:
         maori_word = maori_word[:-1]
         
+    #clean and split into multiple words if multiple spellings.
     maori_word = maori_word.strip()
-    
     maori_word = re.sub(r'\. |=|\.\—', ',', maori_word)
-    
     maori_words = [word.strip() for word in maori_word.split(",") if word.strip()]
     
+    # checking for part of speach tags ensures no garbage gets let through (again, inconsistent content inside html tags.)
     maori_words = [word for word in maori_words if word not in pos_tags and not word.isnumeric()]
     
+    # return maori word(s)
     if maori_words:
         return maori_words
     else:
