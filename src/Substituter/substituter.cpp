fcbb6d980f1b2def1339b111f99bf253a635179c
Jake Norton <jake.norton88@gmail.com>
2023-08-23 11:30:02 +1200

    more coverage tests

--- src/Substituter/substituter.cpp
+++ src/Substituter/substituter.cpp
@@ -5,3 +5,6 @@ void test_func()
 {
     std::cout << "Substituter extern test declaration" << std::endl;
 }
+uint32_t factorial2(uint32_t number) {
+    return number <= 1 ? number : factorial2(number - 1) * number;
+}
