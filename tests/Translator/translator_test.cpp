6d622c5f859de82a01eedc732fd5dd4eaae8a587
Jake Norton <jake.norton88@gmail.com>
2023-09-14 15:51:42 +1200

    changed testing to use root of build directory to fix windowsissues

--- tests/Translator/translator_test.cpp
+++ tests/Translator/translator_test.cpp
@@ -14,13 +14,13 @@
 TEST_CASE("TestPosTaggingOnValidFile", "[get_pos_tags]")
 {
     // Get the POS tags for a known valid test file
-    std::vector<std::string> hound_test_output = get_pos_tags("./pos_data/hound");
+    std::vector<std::string> hound_test_output = get_pos_tags("hound");
 
     // Vector to hold the expected results for the valid file
     std::vector<std::string> succeed_case;
 
     // Open the expected results file for the valid test case
-    std::string pos_path = "./pos_data/hound.test";
+    std::string pos_path = "hound.test";
     std::ifstream file(pos_path);
     std::string tag;
 
@@ -56,37 +56,13 @@ TEST_CASE("TestPosTaggingOnInvalidFile", "[get_pos_tags]")
     REQUIRE(fail_case_output == fail_case);
 }
 
-// TEST_CASE("TestTranslate", "[translate_and_replace]")
-// {
-//     // Open the expected results file for the valid test case
-//     std::string translated_path = "./test_data/translated.test";
-//     std::ifstream file(translated_path); // Use translated_path instead of pos_path
-//     std::string succeed_case;
-//     if (file) {
-//         succeed_case = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-//         // Now, succeed_case contains the content of the file as a string
-//     } else {
-//         // Handle error, e.g., file not found or other read errors
-//         FAIL("Expected results file not found or couldn't be read.");
-//     }
-
-//     std::ifstream test_file("./test_data/test.txt");
-//     std::string translate_test_output = translate_and_replace(test_file);
-
-//     // // Ensure the output matches the expected result for the invalid file
-//     // INFO("Expected: " << succeed_case);
-//     // INFO("Actual: " << translate_test_output);
-//     REQUIRE(succeed_case == translate_test_output);
-// }
-
-
 TEST_CASE("Test translate_and_replace", "[translate_and_replace]") {
     // Read expected output from file
-    std::ifstream expectedFile("./test_data/translated.test");
+    std::ifstream expectedFile("translated.test");
     std::string expectedOutput((std::istreambuf_iterator<char>(expectedFile)), std::istreambuf_iterator<char>());
 
     // Prepare input for translate_and_replace
-    std::ifstream inputFile("./test_data/test.txt");
+    std::ifstream inputFile("test.txt");
     std::stringstream inputStream;
     inputStream << inputFile.rdbuf();
 
