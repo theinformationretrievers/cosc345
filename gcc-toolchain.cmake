99cf8ce26642278387c9da820c30b7c603925e35
Jake Norton <jake.norton88@gmail.com>
2023-09-21 15:04:35 +1200

    updated ubuntu ci to work with the changes

--- gcc-toolchain.cmake
+++ gcc-toolchain.cmake
@@ -1,1 +1,4 @@
-set(CMAKE_CXX_COMPILER "/usr/bin/g++-13")
+execute_process(COMMAND which g++-13
+                OUTPUT_VARIABLE GPP_PATH
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+set(CMAKE_CXX_COMPILER "${GPP_PATH}")
