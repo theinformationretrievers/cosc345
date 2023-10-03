e6ace09ae5c57c32c051f0b02616103b777769c0
Matt Tyler <tylma141@student.otago.ac.nz>
2023-10-04 07:39:12 +1300

    Reset scroll position after page transition

--- src/GUI/MyApp.cpp
+++ src/GUI/MyApp.cpp
@@ -22,21 +22,19 @@
 #define WINDOW_HEIGHT 900
 #define BUFSIZE MAX_PATH
 
-
-
 MyApp::MyApp()
 {
-  ///
-  /// Create our main App instance.
-  ///
-  app_ = App::Create();
+    ///
+    /// Create our main App instance.
+    ///
+    app_ = App::Create();
 
     ///
     /// Create a resizable window by passing by OR'ing our window flags with
     /// kWindowFlags_Resizable.
     ///
     window_ = Window::Create(app_->main_monitor(), WINDOW_WIDTH, WINDOW_HEIGHT,
-        false, kWindowFlags_Titled | kWindowFlags_Resizable);
+                             false, kWindowFlags_Titled | kWindowFlags_Resizable);
 
     ///
     /// Create our HTML overlay-- we don't care about its initial size and
@@ -79,7 +77,7 @@ MyApp::MyApp()
     overlay_->view()->set_view_listener(this);
 }
 
-MyApp::~MyApp() { }
+MyApp::~MyApp() {}
 
 void MyApp::Run() { app_->Run(); }
 
@@ -90,14 +88,14 @@ void MyApp::Run() { app_->Run(); }
  *					 and returns the file path
  * @returns The file path as a JSValue
  */
-JSValue MyApp::GetFileWindows(const JSObject& thisObject, const JSArgs& args)
+JSValue MyApp::GetFileWindows(const JSObject &thisObject, const JSArgs &args)
 {
     HWND hwnd = (HWND)window_->native_handle();
     TCHAR szFilter[] = TEXT("Text Files (*.TXT)\0*.txt\0");
 
     OPENFILENAME ofn; // common dialog box structure
     char szFile[260]; // buffer for file name
-    HANDLE hf; // file handle
+    HANDLE hf;        // file handle
 
     // Initialize OPENFILENAME
     ZeroMemory(&ofn, sizeof(ofn));
@@ -116,10 +114,13 @@ JSValue MyApp::GetFileWindows(const JSObject& thisObject, const JSArgs& args)
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 
     // Display the Open dialog box.
-    if (GetOpenFileName(&ofn) == TRUE) {
+    if (GetOpenFileName(&ofn) == TRUE)
+    {
         hf = CreateFile(ofn.lpstrFile, GENERIC_READ, 0, (LPSECURITY_ATTRIBUTES)NULL,
-            OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
-    } else {
+                        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, (HANDLE)NULL);
+    }
+    else
+    {
         std::cerr << "Error opening file" << std::endl;
         std::string err("unable to open file");
         return JSValue(err.c_str());
@@ -144,18 +145,20 @@ reads the contents
            error as a JSValue string
 *
 */
-JSValue MyApp::GetFileLinux(const JSObject& thisObject, const JSArgs& args)
+JSValue MyApp::GetFileLinux(const JSObject &thisObject, const JSArgs &args)
 {
     std::array<char, 128> buffer;
     std::string result = "";
 
-    FILE* pipe = popen("./file_chooser", "r");
-    if (!pipe) {
+    FILE *pipe = popen("./file_chooser", "r");
+    if (!pipe)
+    {
         std::cerr << "Couldn't start command." << std::endl;
         return 0;
     }
 
-    while (fgets(buffer.data(), 128, pipe) != nullptr) {
+    while (fgets(buffer.data(), 128, pipe) != nullptr)
+    {
         result += buffer.data();
     }
 
@@ -173,9 +176,10 @@ JSValue MyApp::GetFileLinux(const JSObject& thisObject, const JSArgs& args)
            error as a JSValue string
 */
 
-JSValue MyApp::GetTranslatedText(const JSObject& thisObject, const JSArgs& args)
+JSValue MyApp::GetTranslatedText(const JSObject &thisObject, const JSArgs &args)
 {
-    if (!args[0].IsString()) {
+    if (!args[0].IsString())
+    {
         return JSValue("Invalid string");
     }
     double page = args[1].ToNumber();
@@ -184,13 +188,14 @@ JSValue MyApp::GetTranslatedText(const JSObject& thisObject, const JSArgs& args)
     // Extracting the filePath
     JSStringRef jsPathString = JSValueToStringCopy(thisObject.context(), args[0], &exception);
     size_t pathLength = JSStringGetMaximumUTF8CStringSize(jsPathString);
-    char* filePath = new char[pathLength];
+    char *filePath = new char[pathLength];
     JSStringGetUTF8CString(jsPathString, filePath, pathLength);
     std::string strPath = filePath;
 
     // Extracting the status
     delete[] filePath;
-    if (strPath != currentPath || currentPath == "default") {
+    if (strPath != currentPath || currentPath == "default")
+    {
         currentPath = strPath;
         fileStream.close();
         pages = {};
@@ -200,10 +205,11 @@ JSValue MyApp::GetTranslatedText(const JSObject& thisObject, const JSArgs& args)
         fileSize = 1000000;
         fileStream = std::ifstream(strPath);
         fileStream.seekg(0, std::ios::end); // Move to the end of the file
-        fileSize = fileStream.tellg(); // Get the current position (which is the end)
+        fileSize = fileStream.tellg();      // Get the current position (which is the end)
         fileStream.seekg(0, std::ios::beg); // Move back to the beginning or to the original position
 
-        if (!fileStream.is_open()) {
+        if (!fileStream.is_open())
+        {
             std::cerr << "Failed to open the fileStream: " << strPath << std::endl;
             diagnoseFileStream("MyApp::GetTranslatedText - After trying to open file");
             return JSValue("Failed to open the fileStream");
@@ -212,29 +218,36 @@ JSValue MyApp::GetTranslatedText(const JSObject& thisObject, const JSArgs& args)
         translateNextChunk();
     }
     updateReaderContent(page);
-    if (endPage){
+    if (endPage)
+    {
         return JSValue(pages.size());
     }
     return JSValue("normal");
 }
-void MyApp::diagnoseFileStream(const std::string& context)
+void MyApp::diagnoseFileStream(const std::string &context)
 {
     std::cerr << "Diagnostics from: " << context << std::endl;
-    if (!fileStream.is_open()) {
+    if (!fileStream.is_open())
+    {
         std::cerr << "File stream is not open." << std::endl;
-    } else {
+    }
+    else
+    {
         std::cerr << "File stream is open." << std::endl;
     }
 
-    if (fileStream.eof()) {
+    if (fileStream.eof())
+    {
         std::cerr << "End of file reached." << std::endl;
     }
 
-    if (fileStream.fail()) {
+    if (fileStream.fail())
+    {
         std::cerr << "File stream has encountered an error." << std::endl;
     }
 
-    if (fileStream.bad()) {
+    if (fileStream.bad())
+    {
         std::cerr << "File stream has encountered a non-recoverable error." << std::endl;
     }
 
@@ -243,11 +256,12 @@ void MyApp::diagnoseFileStream(const std::string& context)
 
 void MyApp::updateReaderContent(const double page)
 {
-    if (pages.size() <= page) {
+    if (pages.size() <= page)
+    {
         translateNextChunk();
     }
-    std::string jsCode = "document.getElementById('reader-content').innerHTML = `<pre>" + pages[page] + "</pre>`;";
-    // std::cout << jsCode << std::endl;
+    std::string jsCode = "document.getElementById('reader-content').innerHTML = `<pre>" + pages[page] + "</pre>`; document.getElementById('view').scrollTop = 0";
+
     overlay_->view()->EvaluateScript(jsCode.c_str());
 }
 
@@ -255,7 +269,8 @@ void MyApp::translateNextChunk()
 {
     startPosition = fileStream.tellg();
 
-    if ((chunkSize + startPosition) > fileSize) {
+    if ((chunkSize + startPosition) > fileSize)
+    {
         std::streamoff chunkLength = fileSize - startPosition;
         std::vector<char> buffer(chunkLength);
         fileStream.read(buffer.data(), chunkLength);
@@ -265,37 +280,46 @@ void MyApp::translateNextChunk()
         fileStream.seekg(endPosition);
 
         endPage = true;
-    } else {
+    }
+    else
+    {
 
         fileStream.seekg(chunkSize, std::ios::cur); // Move forward by chunkSize
         endPosition = fileStream.tellg();
 
         char ch;
         bool foundEndOfChunk = false;
-        while (fileStream.get(ch)) {
-            if ((ch >= 41 && ch <= 90) || (ch >= 97 && ch <= 122)) {
-            } else {
+        while (fileStream.get(ch))
+        {
+            if ((ch >= 41 && ch <= 90) || (ch >= 97 && ch <= 122))
+            {
+            }
+            else
+            {
                 // This is not a letter, so it's the end of the word
                 endPosition = fileStream.tellg();
                 foundEndOfChunk = true;
                 break;
             }
         }
-        if (!foundEndOfChunk && fileStream.eof()) {
+        if (!foundEndOfChunk && fileStream.eof())
+        {
             endPosition = fileStream.tellg();
         }
         std::streamoff chunkLength = endPosition - startPosition;
 
         // Set the get position to startPosition
         fileStream.seekg(startPosition);
-        if (!fileStream.good()) {
+        if (!fileStream.good())
+        {
             std::cerr << "File stream is not in a good state before reading the chunk." << std::endl;
             diagnoseFileStream("translateNextChunk - Before reading chunk");
         }
         // Read the chunk into a buffer
         std::vector<char> buffer(chunkLength);
         fileStream.read(buffer.data(), chunkLength);
-        if (fileStream.fail()) {
+        if (fileStream.fail())
+        {
             std::cerr << "Failed to read the chunk from the file." << std::endl;
             diagnoseFileStream("translateNextChunk - After reading chunk");
         }
@@ -305,9 +329,7 @@ void MyApp::translateNextChunk()
         pages.push_back(result);
 
         fileStream.seekg(endPosition);
-        
     }
-
 }
 
 /*
@@ -317,16 +339,17 @@ void MyApp::translateNextChunk()
 * @returns The contents of the opened file as a JSValue string or the
            error as a JSValue string
 */
-JSValue MyApp::writeLocalBook(const JSObject& thisObject, const JSArgs& args)
+JSValue MyApp::writeLocalBook(const JSObject &thisObject, const JSArgs &args)
 {
-    if (!args[0].IsString()) {
+    if (!args[0].IsString())
+    {
         return JSValue("Invalid string");
     }
 
     JSValueRef exception = NULL;
     JSStringRef jsPathString = JSValueToStringCopy(thisObject.context(), args[0], &exception);
     size_t pathLength2 = JSStringGetMaximumUTF8CStringSize(jsPathString);
-    char* filePath2 = new char[pathLength2];
+    char *filePath2 = new char[pathLength2];
     JSStringGetUTF8CString(jsPathString, filePath2, pathLength2);
 
     std::string writeFile = "./local_files.txt";
@@ -336,23 +359,30 @@ JSValue MyApp::writeLocalBook(const JSObject& thisObject, const JSArgs& args)
     std::set<std::string> uniqueLines;
     std::string line;
 
-    while (std::getline(inputFile, line)) {
+    while (std::getline(inputFile, line))
+    {
         uniqueLines.insert(line);
     }
 
     // If the new filepath is not already listed
-    if (uniqueLines.find(filePath2) == uniqueLines.end()) {
+    if (uniqueLines.find(filePath2) == uniqueLines.end())
+    {
         // Open the file in append mode
         std::ofstream file(writeFile, std::ios_base::app);
-        if (file.is_open() && filePath2 != "unable to open file") {
+        if (file.is_open() && filePath2 != "unable to open file")
+        {
             // Write the content followed by a newline character
             file << filePath2 << "\n";
             file.close();
             std::cout << "Content '" << filePath2 << "' written to file '" << writeFile << "'." << std::endl;
-        } else {
+        }
+        else
+        {
             std::cerr << "Error: Unable to open file '" << writeFile << "' for writing." << std::endl;
         }
-    } else {
+    }
+    else
+    {
         return JSValue("Duplicate");
     }
 
@@ -365,7 +395,7 @@ JSValue MyApp::writeLocalBook(const JSObject& thisObject, const JSArgs& args)
 * @returns The contents of the opened file as a JSValue string or the
            error as a JSValue string
 */
-JSValue MyApp::getPreviousLocalFiles(const JSObject& thisObject, const JSArgs& args)
+JSValue MyApp::getPreviousLocalFiles(const JSObject &thisObject, const JSArgs &args)
 {
     std::string writeFile = "./local_files.txt";
 
@@ -374,13 +404,15 @@ JSValue MyApp::getPreviousLocalFiles(const JSObject& thisObject, const JSArgs& a
     std::set<std::string> uniqueLines;
     std::string line;
 
-    while (std::getline(inputFile, line)) {
+    while (std::getline(inputFile, line))
+    {
         uniqueLines.insert(line);
     }
 
     ultralight::JSArray jsArray;
 
-    for (const std::string& str : uniqueLines) {
+    for (const std::string &str : uniqueLines)
+    {
         // Convert each string to a JSValue and add it to the JSArray
         ultralight::JSValue jsString = ultralight::JSValue(str.c_str());
         jsArray.push(jsString);
@@ -398,10 +430,10 @@ void MyApp::OnUpdate()
     ///
 }
 
-void MyApp::OnClose(ultralight::Window* window) { app_->Quit(); }
+void MyApp::OnClose(ultralight::Window *window) { app_->Quit(); }
 
-void MyApp::OnResize(ultralight::Window* window, uint32_t width,
-    uint32_t height)
+void MyApp::OnResize(ultralight::Window *window, uint32_t width,
+                     uint32_t height)
 {
     ///
     /// This is called whenever the window changes size (values in pixels).
@@ -411,16 +443,16 @@ void MyApp::OnResize(ultralight::Window* window, uint32_t width,
     overlay_->Resize(width, height);
 }
 
-void MyApp::OnFinishLoading(ultralight::View* caller, uint64_t frame_id,
-    bool is_main_frame, const String& url)
+void MyApp::OnFinishLoading(ultralight::View *caller, uint64_t frame_id,
+                            bool is_main_frame, const String &url)
 {
     ///
     /// This is called when a frame finishes loading on the page.
     ///
 }
 
-void MyApp::OnDOMReady(ultralight::View* caller, uint64_t frame_id,
-    bool is_main_frame, const String& url)
+void MyApp::OnDOMReady(ultralight::View *caller, uint64_t frame_id,
+                       bool is_main_frame, const String &url)
 {
     ///
     /// This is called when a frame's DOM has finished loading on the page.
@@ -443,11 +475,11 @@ void MyApp::OnDOMReady(ultralight::View* caller, uint64_t frame_id,
 
     // Call the openRecent() JavaScript function when the DOM is ready
     // this is for when the app first loads, as it doesn't seem to work when placed inside app.js
-    ultralight::String* x = NULL;
+    ultralight::String *x = NULL;
     caller->EvaluateScript("openRecent();", x);
 }
 
-void MyApp::OnChangeCursor(ultralight::View* caller, Cursor cursor)
+void MyApp::OnChangeCursor(ultralight::View *caller, Cursor cursor)
 {
     ///
     /// This is called whenever the page requests to change the cursor.
@@ -457,7 +489,7 @@ void MyApp::OnChangeCursor(ultralight::View* caller, Cursor cursor)
     window_->SetCursor(cursor);
 }
 
-void MyApp::OnChangeTitle(ultralight::View* caller, const String& title)
+void MyApp::OnChangeTitle(ultralight::View *caller, const String &title)
 {
     ///
     /// This is called whenever the page requests to change the title.
@@ -466,19 +498,20 @@ void MyApp::OnChangeTitle(ultralight::View* caller, const String& title)
     ///
     window_->SetTitle(title.utf8().data());
 }
-void MyApp::OnAddConsoleMessage(ultralight::View* caller,
-    ultralight::MessageSource source,
-    ultralight::MessageLevel level,
-    const ultralight::String& message,
-    uint32_t line_number,
-    uint32_t column_number,
-    const ultralight::String& source_id)
+void MyApp::OnAddConsoleMessage(ultralight::View *caller,
+                                ultralight::MessageSource source,
+                                ultralight::MessageLevel level,
+                                const ultralight::String &message,
+                                uint32_t line_number,
+                                uint32_t column_number,
+                                const ultralight::String &source_id)
 {
 
     std::cout << "[Console]: [" << Stringify(source) << "] ["
               << Stringify(level) << "] " << ToUTF8(message);
 
-    if (source == kMessageSource_JS) {
+    if (source == kMessageSource_JS)
+    {
         std::cout << " (" << ToUTF8(source_id) << " @ line " << line_number
                   << ", col " << column_number << ")";
     }
