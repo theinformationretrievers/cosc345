57b64bed460e8125b77790628d53eadbe15e917d
Matt Tyler <tylma141@student.otago.ac.nz>
2023-10-04 14:35:31 +1300

    Shifted profile settings to database
    Can now store word specific settings in the database. Still needs tuning

--- src/Translator/translator.cpp
+++ src/Translator/translator.cpp
@@ -12,7 +12,12 @@
 
 #pragma execution_character_set("utf-8")
 
-void strip_and_lower(std::string& str)
+#define IS_UTF8_ALPHABET(ch) ((ch >= 41 && ch <= 90) || (ch >= 97 && ch <= 122))
+#define IS_UTF8_LOWERCASE_LETTER(ch) (ch >= 97 && ch <= 122)
+
+#define GLOBAL_REPLACEMENT_RATE 5
+
+void strip_and_lower(std::string &str)
 {
     str.erase(std::remove_if(str.begin(), str.end(),
                              [](unsigned char c)
@@ -31,63 +36,54 @@ double should_translate(int user_encounter_count, double word_preference, double
     return 1.0 / (1.0 + std::exp(-introduction_rate * (user_encounter_count - word_preference)));
 }
 
-std::string translate_and_replace(std::istream& stream, int seed)
+std::string translate_and_replace(std::istream &stream, int seed)
 {
-    Dictionary dict("./dict.sqlite");
-    
-    std::unordered_map<std::string, int> encounter_map;
-    std::unordered_map<std::string, std::string> translation_cache;
+    static Dictionary dict("./dict.sqlite");
+    static std::uniform_real_distribution<> dis(0.0, 1.0);
 
-    // std::random_device rd;
     std::mt19937 gen(42);
-    std::uniform_real_distribution<> dis(0.0, 1.0);
 
     std::stringstream output;
     std::string word_buffer;
     char ch;
 
-    auto process_word = [&](std::string& word) {
+    auto process_word = [&](std::string &word)
+    {
         std::string original = word;
         strip_and_lower(word);
 
-        std::string maori_translation;
-        auto cached_translation = translation_cache.find(word);
+        translation maori_translation = dict.translate(word);
 
-        if (cached_translation != translation_cache.end())
+        if (maori_translation.maori_word != "!")
         {
-            maori_translation = cached_translation->second;
-        } else {
 
-            auto translation = dict.translate(word);
-
-            if (translation.size()) {
-                maori_translation = translation.front();
-                translation_cache[word] = maori_translation;
-            } else {
-                maori_translation = "!";
-                translation_cache[word] = maori_translation;
-            }
-        }
-
-        if (maori_translation != "!") {
-            int encountered = encounter_map[word]++;
-            if (dis(gen) <= should_translate(encountered, 1, 5))
+            if (dis(gen) <= should_translate(maori_translation.encounter_rate, maori_translation.word_preference, GLOBAL_REPLACEMENT_RATE))
             {
-                output << "<span class=\"maori-word tooltip\">" << maori_translation << "<span class=\"tooltiptext\">" << original << "</span></span>";
-            } else {
+                if (IS_UTF8_LOWERCASE_LETTER(original[0]))
+                {
+                    maori_translation.maori_word[0] = std::tolower(maori_translation.maori_word[0]);
+                }
+
+                output << "<span class='maori-word tooltip'>" << maori_translation.maori_word << "<span class='tooltiptext'>" << original << "</span></span>";
+            }
+            else
+            {
                 output << original;
             }
-        } else {
+        }
+        else
+        {
             output << original;
         }
     };
 
     while (stream.get(ch))
     {
-        if ((ch >= 41 && ch <= 90) || (ch >= 97 && ch <= 122))
+        if (IS_UTF8_ALPHABET(ch))
         {
             word_buffer.push_back(ch);
-        } else
+        }
+        else
         {
             if (!word_buffer.empty())
             {
@@ -95,19 +91,26 @@ std::string translate_and_replace(std::istream& stream, int seed)
                 word_buffer.clear();
             }
 
-            if (ch == '\n') {
+            if (ch == '\n')
+            {
                 output << "</p>\n<br><p>";
             }
-            else {
+            else
+            {
                 output << ch; // Append non-alpha character to output
             }
         }
     }
 
     // Handle the case where the stream ends with a word
-    if (!word_buffer.empty()) {
+    if (!word_buffer.empty())
+    {
         process_word(word_buffer);
     }
+
+    // std::cout << output.str() << std::endl;
+
+    // dict.update_translations(translation_cache);
 
     return output.str();
 }
